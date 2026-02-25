135d63ce1d8c5a296751ccf5aa1d082fb54a7d1f
Felix Maiko <maikofelix89@gmail.com>
2026-02-25 18:11:28 +0300

    (improv) OMRS3-156: Health Worker License Updates (#6)

--- src/hie/health-worker/banner/health-worker-banner.tsx
+++ src/hie/health-worker/banner/health-worker-banner.tsx
@@ -1,24 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { useSession } from '@openmrs/esm-framework';
+import React, { useEffect, useMemo, useState } from 'react';
+import { showSnackbar, useSession } from '@openmrs/esm-framework';
 import { getProviderByUuid } from '../../../shared/provider.resource';
 import { searchPractitioner } from '../../hie-resource';
-import { type TagType, type PractitionerMessage, type PractitionerSearchParams } from '../../../types';
+import {
+  type TagType,
+  type PractitionerMessage,
+  type PractitionerSearchParams,
+  type PractitionerLicense,
+} from '../../../types';
 import styles from './health-worker-banner.scss';
-import { Button, Tag } from '@carbon/react';
+import { Button, InlineLoading, Tag } from '@carbon/react';
 import HealthWorkerModal from '../modal/health-worker-details.modal';
 import { getTagType } from '../../../shared/utils/get-tag-type';
 interface HealthWorkerBannerProps {}
 const HealthWorkerBanner: React.FC<HealthWorkerBannerProps> = () => {
   const [practitioner, setPractitioner] = useState<PractitionerMessage>();
   const [displayDetailsModal, setDisplayDetailsModal] = useState<boolean>();
+  const [loading, setLoading] = useState<boolean>(false);
   const session = useSession();
   const locationUuid = session.sessionLocation.uuid;
+  const latestLicense = useMemo(() => getLatestLicense(), [practitioner]);
 
   useEffect(() => {
     getProviderDetails();
   }, []);
 
-  async function getProviderDetails() {
+  async function getProviderDetails(refresh?: boolean) {
     const providerUuid = session.currentProvider.uuid;
     const provider = await getProviderByUuid(providerUuid);
     const { nationalId, licenseNo } = getProviderAttributes(provider);
@@ -28,9 +35,15 @@ const HealthWorkerBanner: React.FC<HealthWorkerBannerProps> = () => {
     } else if (licenseNo) {
       searchParams['licenseNumber'] = licenseNo.value;
     }
+    if (refresh) {
+      searchParams['refresh'] = refresh;
+    }
     if (nationalId || licenseNo) {
+      setLoading(true);
       const practitioner = await searchPractitioner(searchParams, locationUuid);
       setPractitioner(practitioner);
+      checkLicenseValidity(practitioner.licenses);
+      setLoading(false);
     }
   }
   function getProviderAttributes(provider) {
@@ -59,6 +72,67 @@ const HealthWorkerBanner: React.FC<HealthWorkerBannerProps> = () => {
   function showDetailsModal() {
     setDisplayDetailsModal(true);
   }
+  function syncHealthWorkerDetails() {
+    getProviderDetails(true);
+  }
+  const isWithin30Days = (targetDate: string) => {
+    const today = new Date().getMilliseconds();
+    const target = new Date(targetDate).getMilliseconds();
+
+    const diffInMs = target - today;
+    const thirtyDaysInMs = 30 * 24 * 60 * 60 * 1000;
+
+    return diffInMs > 0 && diffInMs <= thirtyDaysInMs;
+  };
+  const isExpired = (licenseEndDate: string): boolean => {
+    return new Date(licenseEndDate).getTime() < new Date().getTime();
+  };
+  function checkLicenseValidity(practitionerLicenses: PractitionerLicense[]) {
+    // check all licenses for a valid license
+    const validLicense = practitionerLicenses.some((l) => {
+      return !isExpired(l.license_end);
+    });
+    if (!validLicense) {
+      // get the latest license
+      const sortedExpiredLicenses = practitionerLicenses.sort((a, b) => {
+        return new Date(b.license_end).getTime() - new Date(a.license_end).getTime();
+      });
+      if (sortedExpiredLicenses && sortedExpiredLicenses) {
+        const latestExpiredLicense = sortedExpiredLicenses[0];
+        showSnackbar({
+          title: `Practioner Licence ${latestExpiredLicense.id} expired`,
+          subtitle: `License ${latestExpiredLicense.id} (${latestExpiredLicense.license_type}) expired on ${latestExpiredLicense.license_end}. Kindly renew. `,
+          kind: 'error',
+        });
+      }
+    } else {
+      //check current valid license if they are 30 days from expiry
+      const nearExpiryLicensed = practitionerLicenses.filter((l) => {
+        return new Date(l.license_end) > new Date() && isWithin30Days(l.license_end);
+      });
+      if (nearExpiryLicensed.length > 0) {
+        const latestNearlyExpired = nearExpiryLicensed[0];
+        showSnackbar({
+          title: `Practioner Licence ${latestNearlyExpired.id} is almost expiring`,
+          subtitle: `License ${latestNearlyExpired.id} (${latestNearlyExpired.license_type}) expires on ${latestNearlyExpired.license_end}. Kindly make plans to renew.`,
+          kind: 'warning-alt',
+        });
+      }
+    }
+  }
+  function getLatestLicense(): PractitionerLicense {
+    if (!practitioner || !practitioner.licenses) {
+      return null;
+    }
+    const sortedLicenses = practitioner.licenses.sort((a, b) => {
+      return new Date(b.license_end).getTime() - new Date(a.license_end).getTime();
+    });
+    if (sortedLicenses.length) {
+      return sortedLicenses[0];
+    } else {
+      return null;
+    }
+  }
   if (!practitioner) {
     return <></>;
   }
@@ -77,22 +151,54 @@ const HealthWorkerBanner: React.FC<HealthWorkerBannerProps> = () => {
             title="Status"
             type={getPractionerStatusType(practitioner.membership.status)}
           >
-            {practitioner.membership.status}
+            Membership: {practitioner.membership.status}
           </Tag>
         </div>
         <div>
           <Tag className="some-class" size="md" title="Active" type={getTagType(practitioner.membership.is_active)}>
-            {practitioner.membership.is_active ? 'Active' : 'Not Active'}
+            Membership: {practitioner.membership.is_active ? 'Active' : 'Not Active'}
           </Tag>
         </div>
         <div>
           <Tag className="some-class" size="md" title="Expiry" type="gray">
-            License Expiry in {practitioner.membership.license_expires_in_days} days
+            Membership Expiry in {practitioner.membership.license_expires_in_days} days
           </Tag>
         </div>
+        {latestLicense ? (
+          <>
+            <div>
+              <Tag className="some-class" size="md" title="Expiry" type="blue">
+                License {latestLicense.id}
+              </Tag>
+            </div>
+            <div>
+              <Tag
+                className="some-class"
+                size="md"
+                title="Expiry"
+                type={getTagType(isExpired(latestLicense.license_end) ? 0 : 1)}
+              >
+                License Expiry {latestLicense.license_end}
+              </Tag>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
         <div>
           <Button size="sm" kind="tertiary" onClick={showDetailsModal}>
             View
+          </Button>
+        </div>
+        <div>
+          <Button size="sm" kind="secondary" onClick={syncHealthWorkerDetails}>
+            {loading ? (
+              <>
+                <InlineLoading description="Syncing..." />
+              </>
+            ) : (
+              <>Sync</>
+            )}
           </Button>
         </div>
         {practitioner && displayDetailsModal ? (
--- src/shared/utils/get-tag-type.ts
+++ src/shared/utils/get-tag-type.ts
@@ -1,7 +1,7 @@
 import { type TagType } from '../../types';
 
-export function getTagType(val: number | string): TagType {
-  if (val === 1 || val === '1') {
+export function getTagType(val: number | string | boolean): TagType {
+  if (val === 1 || val === '1' || val === true) {
     return 'green';
   } else {
     return 'red';
